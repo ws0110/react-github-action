f65174c585dcba098f117a3640f90e1ebd46ffe1
ws0110 <ws010110@gmail.com>
2023-11-19 08:25:26 +0900

    commit 2

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# react testcase sample code
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,58 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("the counter starts at 0", () => {
+  render(<App />); // 컴포넌트 렌더링
+
+  //screen object를 이용해서 원하는 엘레멘트에 접근(접근할 때  ID로 접근)
+  // https://testing-library.com/docs/queries/about
+  const counterElement = screen.getByTestId("counter");
+
+  // id가 counter인 엘레멘트의 텍스트가 0인지 테스트
+  // expect(counterElement).toBe(0);  ==> reaceived가 엘리먼트 전체로 실패하게 됨
+  expect(counterElement).toHaveTextContent(0);
+});
+
+test("minus button has correct test", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const btnElement = screen.getByTestId("minus-btn");
+
+  expect(btnElement).toHaveTextContent("-");
+});
+
+test("plus button has correct test", () => {
+  render(<App />);
+
+  const btnElement = screen.getByTestId("plus-btn");
+
+  expect(btnElement).toHaveTextContent("+");
+});
+
+test("+버튼 클릭 시 카운터 1 증가", () => {
+  render(<App />);
+
+  const btnElement = screen.getByTestId("plus-btn");
+
+  // 버튼 클릭 이벤트 발생
+  fireEvent.click(btnElement);
+
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(1);
+});
+
+test("on/off 버튼 has Blue 컬러", () => {
+  // https://github.com/testing-library/jest-dom : matcher 확인
+  render(<App />);
+  const btnElement = screen.getByTestId("on/off-btn");
+  expect(btnElement).toHaveStyle({ backgroundColor: "blue" }); // CSS 테스트
+});
+
+test("on/off 클릭 시 +,- 버튼 Disalbe", () => {
+  render(<App />);
+  const onOffBtnElement = screen.getByTestId("on/off-btn");
+  fireEvent.click(onOffBtnElement);
+
+  const plusBtnElement = screen.getByTestId("plus-btn");
+  expect(plusBtnElement).toBeDisabled();
 });
